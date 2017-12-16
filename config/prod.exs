73752f687a95aad42b877d5d3fc2c00d12ee76de
rabsztok <rabsztok@gmail.com>
2017-12-16 16:47:37 +0100

    Remove static manifest

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,6 @@ config :game, GameWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "hex-worlds.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
