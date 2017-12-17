1a813f9f2ac771f7cf9ad69d1d127681f2c7b987
rabsztok <rabsztok@gmail.com>
2017-12-17 23:23:45 +0100

    Enable partial map discovery

--- lib/game_web/channels/tile_channel.ex
+++ lib/game_web/channels/tile_channel.ex
@@ -5,14 +5,15 @@ defmodule GameWeb.TileChannel do
   alias Game.Tile
 
   def join("tiles:lobby", %{"world_id" => world_id}, socket) do
-    tiles = Tile.Queries.within_range(world_id, %{x: 0, y: 0, z: 0}, 100)
+    tiles = Tile.Queries.within_range(world_id, %{x: 0, y: 0, z: 0}, 50)
     {:ok, %{tiles: tiles}, socket}
   end
 
-#  def handle_in("move", %{"coordinates" => coordinates, "range" => range}, socket) do
-#    coordinates = Map.new(coordinates, fn {k, v} -> {String.to_atom(k), v} end)
-#    tiles = Tile.Queries.within_range(coordinates, range)
-#    broadcast! socket, "move", %{tiles: tiles}
-#    {:noreply, socket}
-#  end
+
+  def handle_in("move", %{"world_id" => world_id, "coordinates" => coordinates, "range" => range}, socket) do
+    coordinates = Map.new(coordinates, fn {k, v} -> {String.to_atom(k), v} end)
+    tiles = Tile.Queries.within_range(world_id, coordinates, range)
+    broadcast! socket, "move", %{tiles: tiles}
+    {:noreply, socket}
+  end
 end
